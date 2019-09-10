be69fdc0e4fcef7a37ec6fa2cc8621e904f385e0
unknown <bastinda96@gmail.com>
2019-09-10 18:59:45 +0300

    working class code for first case, working code for second

--- MC_C_v0.py
+++ MC_C_v0.py
@@ -1,163 +1,289 @@
 import gym
 import torch.nn as nn
 import torch
-import copy
-import random
 from torch import optim
-from torch.nn import functional as F
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
-env = gym.make("MountainCarContinuous-v0")
-# env.reset()
-#
-# for _ in range(1000):
-#     env.render()
-#     env.step(env.action_space.sample())
-#
-# env.close()
-device = torch.device("cpu")
+from core.buffer import ReplayBuffer
+from core.random_process import OUNoise
+from core.util import hard_update, soft_update, get_class_attr_val
+
+##As an example of code was used #https://github.com/blackredscarf/pytorch-DDPG
+
+class Config:
+    env: str = None
+    episodes: int = None
+    max_steps: int = None
+    max_buff: int = None
+    batch_size: int = None
+    state_dim: int = None
+    state_high = None
+    state_low = None
+    seed = None
+
+    output = 'out'
+
+    action_dim: int = None
+    action_high = None
+    action_low = None
+    action_lim = None
+
+    learning_rate: float = None
+    learning_rate_actor: float = None
+
+    gamma: float = None
+    tau: float = None
+    epsilon: float = None
+    eps_decay = None
+    epsilon_min: float = None
+
+    use_cuda: bool = True
+
+    checkpoint: bool = False
+    checkpoint_interval: int = None
+
+    use_matplotlib: bool = False
+
+    record: bool = False
+    record_ep_interval: int = None
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim, h1=400, h2=300, eps=0.03):
+        super(Critic, self).__init__()
+
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+
+        self.fc1 = nn.Linear(state_dim, h1)
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+
+        self.fc2 = nn.Linear(h1 + action_dim, h2)
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+
+        self.fc3 = nn.Linear(h2, 1)
+        self.fc3.weight.data.uniform_(-eps, eps)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, state, action):
+        s1 = self.relu(self.fc1(state))
+        x = torch.cat((s1, action), dim=1)
+
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
 
 
-def create_new_model():
-    model = nn.Sequential(
-        nn.Linear(2, 32),
-        nn.ReLU(),
-        nn.Linear(32, 32),
-        nn.ReLU(),
-        nn.Linear(32, 1)
-    )
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, h1=400, h2=300, eps=0.03):
+        super(Actor, self).__init__()
 
-    target_model = copy.deepcopy(model)
+        self.state_dim = state_dim
+        self.action_dim = action_dim
 
-    # Инициализация весов нейронной сети
-    def init_weights(layer):
-        if type(layer) == nn.Linear:
-            nn.init.xavier_normal(layer.weight)
+        self.fc1 = nn.Linear(state_dim, h1)
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
 
-    model.apply(init_weights)
+        self.fc2 = nn.Linear(h1, h2)
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
 
-    # Загружаем модель на устройство, определенное в самом начале (GPU или CPU)
-    model.to(device)
-    target_model.to(device)
+        self.fc3 = nn.Linear(h2, action_dim)
+        self.fc3.weight.data.uniform_(-eps, eps)
 
-    # Сразу зададим оптимизатор, с помощью которого будем обновлять веса модели
-    optimizer = optim.Adam(model.parameters(), lr=0.00003)
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
 
-    return model, target_model, optimizer
-
-gamma = 0.99
-def fit(batch, model, target_model, optimizer):
-    state, action, reward, next_state, done = batch
-    # Загружаем батч на выбранное ранее устройство
-    state = torch.tensor(state).to(device).float()
-    next_state = torch.tensor(next_state).to(device).float()
-    reward = torch.tensor(reward).to(device).float()
-    action = torch.tensor(action).to(device)
-    done = torch.tensor(done).to(device)
-
-    # Считаем то, какие значения должна выдавать наша сеть
-    target_q = torch.zeros(reward.size()[0]).float().to(device)
-    with torch.no_grad():
-        # Выбираем максимальное из значений Q-function для следующего состояния
-        target_q[done] = target_model(next_state).max(1)[0].detach()[done]
-    target_q = reward + target_q * gamma
-
-    # Текущее предсказание
-    q = model(state).gather(1, action.unsqueeze(1))
-
-    loss = F.smooth_l1_loss(q, target_q.unsqueeze(1))
-
-    # Очищаем текущие градиенты внутри сети
-    optimizer.zero_grad()
-    # Применяем обратное распространение  ошибки
-    loss.backward()
-    # Ограничиваем значения градиента. Необходимо, чтобы обновления не были слишком большими
-    for param in model.parameters():
-        param.grad.data.clamp_(-1, 1)
-    # Делаем шаг оптимизации
-    optimizer.step()
-
-def select_action(state, epsilon, model, env):
-    if random.random() < epsilon:
-        return env.action_space.sample()
-    a = model(torch.tensor(state).to(device).float().unsqueeze(0))[0].max(0)[1].view(1, 1).item()
-    # print(2, a)
-    return [a]
+    def forward(self, state):
+        x = self.relu(self.fc1(state))
+        x = self.relu(self.fc2(x))
+        action = self.tanh(self.fc3(x)) # tanh limit (-1, 1)
+        return action
 
 
-class Memory:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
+def fanin_init(size, fanin=None):
+    fanin = fanin or size[0]
+    v = 1. / np.sqrt(fanin)
+    return torch.Tensor(size).uniform_(-v, v)
 
-    def push(self, element):
-        """Сохраняет элемент в циклический буфер"""
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = element
-        self.position = (self.position + 1) % self.capacity
+class DDPG:
+    def __init__(self, config):
+        self.config = config
+        self.init()
 
-    def sample(self, batch_size):
-        """Возвращает случайную выборку указанного размера"""
-        return list(zip(*random.sample(self.memory, batch_size)))
+    def init(self):
+        self.state_dim = self.config.state_dim
+        self.action_dim = self.config.action_dim
+        self.batch_size = self.config.batch_size
+        self.gamma = self.config.gamma
+        self.epsilon = self.config.epsilon
+        self.is_training = True
+        self.randomer = OUNoise(self.action_dim)
+        self.buffer = ReplayBuffer(self.config.max_buff)
 
-    def __len__(self):
-        return len(self.memory)
+        self.actor = Actor(self.state_dim, self.action_dim)
+        self.actor_target = Actor(self.state_dim, self.action_dim)
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), self.config.learning_rate_actor)
+
+        self.critic = Critic(self.state_dim, self.action_dim)
+        self.critic_target = Critic(self.state_dim, self.action_dim)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), self.config.learning_rate)
+
+        hard_update(self.actor_target, self.actor)
+        hard_update(self.critic_target, self.critic)
+
+        if self.config.use_cuda:
+            self.cuda()
+
+    def learning(self):
+        s1, a1, r1, t1, s2 = self.buffer.sample_batch(self.batch_size)
+        # bool -> int
+        t1 = (t1 == False) * 1
+        s1 = torch.tensor(s1, dtype=torch.float)
+        a1 = torch.tensor(a1, dtype=torch.float)
+        r1 = torch.tensor(r1, dtype=torch.float)
+        t1 = torch.tensor(t1, dtype=torch.float)
+        s2 = torch.tensor(s2, dtype=torch.float)
+
+        if self.config.use_cuda:
+            s1 = s1.cuda()
+            a1 = a1.cuda()
+            r1 = r1.cuda()
+            t1 = t1.cuda()
+            s2 = s2.cuda()
+
+        a2 = self.actor_target(s2).detach()
+        target_q = self.critic_target(s2, a2).detach()
+        y_expected = r1[:, None] + t1[:, None] * self.config.gamma * target_q
+        y_predicted = self.critic.forward(s1, a1)
+
+        # critic gradient
+        critic_loss = nn.MSELoss()
+        loss_critic = critic_loss(y_predicted, y_expected)
+        self.critic_optimizer.zero_grad()
+        loss_critic.backward()
+        self.critic_optimizer.step()
+
+        # actor gradient
+        pred_a = self.actor.forward(s1)
+        loss_actor = (-self.critic.forward(s1, pred_a)).mean()
+        self.actor_optimizer.zero_grad()
+        loss_actor.backward()
+        self.actor_optimizer.step()
+
+        # Notice that we only have gradient updates for actor and critic, not target
+        # actor_optimizer.step() and critic_optimizer.step()
+
+        soft_update(self.actor_target, self.actor, self.config.tau)
+        soft_update(self.critic_target, self.critic, self.config.tau)
+
+        return loss_actor.item(), loss_critic.item()
+
+    def cuda(self):
+        self.actor.cuda()
+        self.actor_target.cuda()
+        self.critic.cuda()
+        self.critic_target.cuda()
+
+    def decay_epsilon(self):
+        self.epsilon -= self.config.eps_decay
+
+    def get_action(self, state):
+        state = torch.tensor(state, dtype=torch.float).unsqueeze(0)
 
 
-##Количество обновлений model между обновлениями target model
-target_update = 1000
-#Размер одного батча, который на вход принимает модель
-batch_size = 128
-#Количество шагов среды
-max_steps = 100001
-#Границы коэффициента exploration
-max_epsilon = 0.5
-min_epsilon = 0.1
+        if self.config.use_cuda:
+            state = state.cuda()
 
-def run():
-      #Создаем модель и буфер
-      memory = Memory(5000)
-      model, target_model, optimizer = create_new_model()
-      state = env.reset()
-      rewards_by_target_updates = []
-      for step in tqdm(range(max_steps)):
-            #Делаем шаг в среде
-            epsilon = max_epsilon - (max_epsilon - min_epsilon)* step / max_steps
-            action = select_action(state, epsilon, model, env)
-            new_state, reward, done, _ = env.step(action)
-            # modified_reward = reward + 10 * abs(new_state[1])
-            modified_reward = reward + 300 * (gamma * abs(new_state[1]) - abs(state[1]))
-            #Запоминаем опыт и, если нужно, перезапускаем среду
-            memory.push((state, action, modified_reward, new_state, done))
-            if done:
-                  state = env.reset()
-                  done = False
-            else:
-                  state = new_state
+        action = self.actor(state).detach()
+        action = action.squeeze(0).cpu().numpy()
+        action += self.is_training * max(self.epsilon, self.config.epsilon_min) * self.randomer.noise()
+        action = np.clip(action, -1.0, 1.0)
 
-            #Градиентный спуск
-            if step > batch_size:
-                 fit(memory.sample(batch_size), model, target_model, optimizer)
+        self.action = action
+        return action
 
-            if step % target_update == 0:
-                  target_model = copy.deepcopy(model)
+    def reset(self):
+        self.randomer.reset()
 
-                  #Exploitation
-                  state = env.reset()
-                  total_reward = 0
-                  while not done:
-                        action = select_action(state, 0, target_model, env)
-                        state, reward, done, _ = env.step(action)
-                        total_reward += reward
 
-                  state = env.reset()
-                  rewards_by_target_updates.append(total_reward)
-      return rewards_by_target_updates
 
-r = run()
-# print(r)
-plt.plot(r)
-plt.show()
+class MountainCarContinuous:
+
+    def run(self):
+        env = gym.make("MountainCarContinuous-v0")
+        # self.env.reset()
+        # for _ in range(10):
+        #     new_state, reward, done, _ = self.env.step(self.env.action_space.sample())
+        #     print(new_state)
+        config = Config()
+        config.env = env
+        config.gamma = 0.99
+        config.episodes = 1000
+        config.max_steps = 200
+        config.batch_size = 128
+        config.epsilon = 1.0
+        config.eps_decay = 0.001
+        config.max_buff = 1000000
+        config.use_cuda = False
+        config.learning_rate = 1e-3
+        config.learning_rate_actor = 1e-4
+        config.epsilon_min = 0.001
+        config.epsilon = 1.0
+        config.tau = 0.001
+        config.action_dim = int(env.action_space.shape[0])
+        config.action_lim = float(env.action_space.high[0])
+        config.state_dim = int(env.observation_space.shape[0])
+
+        agent = DDPG(config)
+        agent.is_training = True
+        rewards_by_target_updates = []
+        total_step = 0
+
+        for ep in range(0, config.episodes):
+            state0 = env.reset()
+            agent.reset()
+
+            done = False
+            step = 0
+            actor_loss, critics_loss, reward = 0, 0, 0
+
+            # # decay noise
+            agent.decay_epsilon()
+
+            while not done:
+                action = agent.get_action(state0)
+
+                state1, reward1, done, info = env.step(action)
+                agent.buffer.add(state0, action, reward1, done, state1)
+                state0 = state1
+
+                if agent.buffer.size() > config.batch_size:
+                    loss_a, loss_c = agent.learning()
+                    actor_loss += loss_a
+                    critics_loss += loss_c
+
+                reward += reward1
+                step += 1
+                total_step += 1
+
+                if step + 1 > config.max_steps:
+                    break
+
+            rewards_by_target_updates.append(reward)
+            # avg_reward = float(np.mean(rewards_by_target_updates[-100:]))
+            print(f'Epoch {ep}/{config.episodes}, reward {rewards_by_target_updates[-1]:.4f}')
+            # print('Best 100-episodes average reward', ep, avg_reward)
+            #
+            # print('total step: %5d, episodes %3d, episode_step: %5d, episode_reward: %5f' % (
+            #     total_step, ep, step, reward))
+
+        return rewards_by_target_updates
+
+if __name__ == '__main__':
+    MC = MountainCarContinuous()
+    res = MC.run()
+    plt.plot(res)
+    plt.show()
+
--- MC_v0.py
+++ MC_v0.py
@@ -7,74 +7,6 @@ from torch import optim
 from torch.nn import functional as F
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-env = gym.make("MountainCar-v0")
-
-device = torch.device("cpu")
-
-
-def create_new_model():
-    model = nn.Sequential(
-        nn.Linear(2, 32),
-        nn.ReLU(),
-        nn.Linear(32, 32),
-        nn.ReLU(),
-        nn.Linear(32, 3)
-    )
-
-    target_model = copy.deepcopy(model)
-
-    # Инициализация весов нейронной сети
-    def init_weights(layer):
-        if type(layer) == nn.Linear:
-            nn.init.xavier_normal(layer.weight)
-
-    model.apply(init_weights)
-
-    # Загружаем модель на устройство, определенное в самом начале (GPU или CPU)
-    model.to(device)
-    target_model.to(device)
-
-    # Сразу зададим оптимизатор, с помощью которого будем обновлять веса модели
-    optimizer = optim.Adam(model.parameters(), lr=0.00003)
-
-    return model, target_model, optimizer
-
-gamma = 0.99
-def fit(batch, model, target_model, optimizer):
-    state, action, reward, next_state, done = batch
-    # Загружаем батч на выбранное ранее устройство
-    state = torch.tensor(state).to(device).float()
-    next_state = torch.tensor(next_state).to(device).float()
-    reward = torch.tensor(reward).to(device).float()
-    action = torch.tensor(action).to(device)
-    done = torch.tensor(done).to(device)
-
-    # Считаем то, какие значения должна выдавать наша сеть
-    target_q = torch.zeros(reward.size()[0]).float().to(device)
-    with torch.no_grad():
-        # Выбираем максимальное из значений Q-function для следующего состояния
-        target_q[done] = target_model(next_state).max(1)[0].detach()[done]
-    target_q = reward + target_q * gamma
-
-    # Текущее предсказание
-    q = model(state).gather(1, action.unsqueeze(1))
-
-    loss = F.smooth_l1_loss(q, target_q.unsqueeze(1))
-
-    # Очищаем текущие градиенты внутри сети
-    optimizer.zero_grad()
-    # Применяем обратное распространение  ошибки
-    loss.backward()
-    # Ограничиваем значения градиента. Необходимо, чтобы обновления не были слишком большими
-    for param in model.parameters():
-        param.grad.data.clamp_(-1, 1)
-    # Делаем шаг оптимизации
-    optimizer.step()
-
-def select_action(state, epsilon, model):
-    if random.random() < epsilon:
-        return random.randint(0, 2)
-    return model(torch.tensor(state).to(device).float().unsqueeze(0))[0].max(0)[1].view(1, 1).item()
 
 
 class Memory:
@@ -98,57 +30,118 @@ class Memory:
         return len(self.memory)
 
 
-##Количество обновлений model между обновлениями target model
-target_update = 1000
-#Размер одного батча, который на вход принимает модель
-batch_size = 128
-#Количество шагов среды
-max_steps = 100001
-#Границы коэффициента exploration
-max_epsilon = 0.5
-min_epsilon = 0.1
+class MountainCarContinuous:
 
-def run():
-      #Создаем модель и буфер
-      memory = Memory(5000)
-      model, target_model, optimizer = create_new_model()
-      state = env.reset()
-      rewards_by_target_updates = []
-      for step in tqdm(range(max_steps)):
-            #Делаем шаг в среде
-            epsilon = max_epsilon - (max_epsilon - min_epsilon)* step / max_steps
-            action = select_action(state, epsilon, model)
+
+
+    def create_new_model(self):
+        def init_weights(layer):
+            if type(layer) == nn.Linear:
+                nn.init.xavier_normal(layer.weight)
+
+        model = nn.Sequential(
+            nn.Linear(2, 32),
+            nn.ReLU(),
+            nn.Linear(32, 32),
+            nn.ReLU(),
+            nn.Linear(32, 3)
+        )
+        target_model = copy.deepcopy(model)
+
+        model.apply(init_weights)
+
+        # Загружаем модель на устройство, определенное в самом начале (GPU или CPU)
+        model.to(self.device)
+        target_model.to(self.device)
+
+        # Сразу зададим оптимизатор, с помощью которого будем обновлять веса модели
+        optimizer = optim.Adam(model.parameters(), lr=0.00003)
+
+        return model, target_model, optimizer
+
+    def __init__(self, _device="cpu", _gamma=0.99):
+        # self.env = gym.make("MountainCarContinuous-v0")
+        # self.env.reset()
+        self.device = torch.device(_device) #gpu
+        self.gamma = _gamma
+
+    def fit(self, batch, model, target_model, optimizer):
+        state, action, reward, next_state, done = batch
+
+        state = torch.tensor(state).to(self.device).float()
+        next_state = torch.tensor(next_state).to(self.device).float()
+        reward = torch.tensor(reward).to(self.device).float()
+        action = torch.tensor(action).to(self.device)
+        done = torch.tensor(done).to(self.device)
+
+        target_q = torch.zeros(reward.size()[0]).float().to(self.device)
+        with torch.no_grad():
+            target_q[done] = target_model(next_state).max(1)[0].detach()[done]
+        target_q = reward + target_q * self.gamma
+
+        q = model(state).gather(1, action.unsqueeze(1))
+
+        loss = F.smooth_l1_loss(q, target_q.unsqueeze(1))
+
+        optimizer.zero_grad()
+        loss.backward()
+        for param in model.parameters():
+            param.grad.data.clamp_(-1, 1)
+        optimizer.step()
+
+    def select_action(self, state, epsilon, model):
+        if random.random() < epsilon:
+            return random.randint(0, 2)
+        return model(torch.tensor(state).to(self.device).float().unsqueeze(0))[0].max(0)[1].view(1, 1).item()
+
+    def run(self):
+        env = gym.make("MountainCar-v0")
+        # self.env.reset()
+        # for _ in range(10):
+        #     new_state, reward, done, _ = self.env.step(self.env.action_space.sample())
+        #     print(new_state)
+        target_update = 1000
+        batch_size = 128
+        max_steps = 10000
+        max_epsilon = 0.5
+        min_epsilon = 0.1
+        memory = Memory(5000)
+        model, target_model, optimizer = self.create_new_model()
+        state = env.reset()
+        rewards_by_target_updates = []
+        for step in tqdm(range(max_steps)):
+            epsilon = max_epsilon - (max_epsilon - min_epsilon) * step / max_steps
+            action = self.select_action(state, epsilon, model)
             new_state, reward, done, _ = env.step(action)
-            # modified_reward = reward + 10 * abs(new_state[1])
-            modified_reward = reward + 300 * (gamma * abs(new_state[1]) - abs(state[1]))
-            #Запоминаем опыт и, если нужно, перезапускаем среду
+
+            modified_reward = reward + 273 * (self.gamma * abs(new_state[1]) - abs(state[1]))
+
             memory.push((state, action, modified_reward, new_state, done))
             if done:
-                  state = env.reset()
-                  done = False
+                state = env.reset()
+                done = False
             else:
-                  state = new_state
+                state = new_state
 
-            #Градиентный спуск
             if step > batch_size:
-                 fit(memory.sample(batch_size), model, target_model, optimizer)
+                self.fit(memory.sample(batch_size), model, target_model, optimizer)
 
             if step % target_update == 0:
-                  target_model = copy.deepcopy(model)
+                target_model = copy.deepcopy(model)
 
-                  #Exploitation
-                  state = env.reset()
-                  total_reward = 0
-                  while not done:
-                        action = select_action(state, 0, target_model)
-                        state, reward, done, _ = env.step(action)
-                        total_reward += reward
+                state = env.reset()
+                total_reward = 0
+                while not done:
+                    action = self.select_action(state, 0, target_model)
+                    state, reward, done, _ = env.step(action)
+                    total_reward += reward
 
-                  state = env.reset()
-                  rewards_by_target_updates.append(total_reward)
-      return rewards_by_target_updates
+                state = env.reset()
+                rewards_by_target_updates.append(total_reward)
+        return rewards_by_target_updates
 
-r = run()
-# print(r)
-plt.plot(r)
-plt.show()
+if __name__ == '__main__':
+    MC = MountainCarContinuous()
+    res = MC.run()
+    plt.plot(res)
+    plt.show()
